68f29a3d4e0e52ecca7961b17040c3491d61b21e
Logorkill <46035412+Logorkill@users.noreply.github.com>
2023-03-15 15:40:00 +0100

    Update Section.java

--- Section.java
+++ Section.java
@@ -1,38 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Section {
-    private final int index;
-    private final int[] shelves;
+    private final String name;
+    private final List<Book> books;
 
-    public Section(int index, int[] shelves) {
-        this.index = index;
-        this.shelves = shelves;
+    public Section(String name) {
+        this.name = name;
+        this.books = new ArrayList<>();
     }
 
-    public synchronized boolean hasBooks(Book book) {
-        for (int i = index * 100; i < (index + 1) * 100; i++) {
-            if (shelves[i] == book.ordinal() + 1) {
-                return true;
-            }
-        }
-        return false;
+    public String getName() {
+        return name;
     }
 
-    public synchronized boolean takeBook(Book book) {
-        for (int i = index * 100; i < (index + 1) * 100; i++) {
-            if (shelves[i] == book.ordinal() + 1) {
-                shelves[i] = 0;
-                return true;
-            }
+    public synchronized void stockBooks(List<Book> books) throws InterruptedException {
+        System.out.println("<" + Bookstore.getTickCount() + "> <" + Thread.currentThread().getName() + "> began stocking " + name + " section with " + books.size() + " books");
+        for (Book book : books) {
+            Thread.sleep(1);
+            this.books.add(book);
         }
-        return false;
+        System.out.println("<" + Bookstore.getTickCount() + "> <" + Thread.currentThread().getName() + "> finished stocking " + name + " section with " + books.size() + " books");
     }
 
-    public synchronized boolean putBook(Book book) {
-        for (int i = index * 100; i < (index + 1) * 100; i++) {
-            if (shelves[i] == 0) {
-                shelves[i] = book.ordinal() + 1;
-                return true;
-            }
+    public synchronized boolean hasBooks() {
+        return !books.isEmpty();
+    }
+
+    public synchronized Book takeBook() throws InterruptedException {
+        while (!hasBooks()) {
+            wait();
         }
-        return false;
+        Thread.sleep(1);
+        Book book = books.remove(0);
+        System.out.println("<" + Bookstore.getTickCount() + "> <" + Thread.currentThread().getName() + "> took a " + book.getGenre() + " book from " + name + " section");
+        notifyAll();
+        return book;
+    }
+
+    public synchronized void returnBook(Book book) {
+        Thread.sleep(1);
+        books.add(book);
+        System.out.println("<" + Bookstore.getTickCount() + "> <" + Thread.currentThread().getName() + "> returned a " + book.getGenre() + " book to " + name + " section");
+        notifyAll();
     }
 }
